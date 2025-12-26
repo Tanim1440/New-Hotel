e77666eefe1870749a64730ccd381bbb51725d4b
Tanim1440 <mtanimkhan520@gmail.com>
2025-12-27 01:05:12 +0600

    adding feature

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,48 +1,3 @@
-// import { Routes, Route, useLocation } from "react-router-dom";
-// import Navber from "./components/Navber";
-// import Home from "./components/Home";
-// import Footer from "./components/Footer";
-// import AllRooms from "./components/pages/AllRooms";
-// import RoomDetails from "./components/pages/RoomDetails";
-// import MyBookings from "./components/pages/MyBookings";
-// import HotelReg from "./components/HotelReg";
-// import Layout from "./components/pages/hotelOwner/Layout";
-// import Dashboard from "./components/pages/hotelOwner/Dashboard";
-// import AddRoom from "./components/pages/hotelOwner/AddRoom";
-// import ListRoom from "./components/pages/hotelOwner/ListRoom";
-
-// const App = () => {
-//   const location = useLocation();
-//   const isOwnerPath = location.pathname.startsWith("/owner");
-
-//   return (
-//     <>
-//       {/* Navbar only for user side */}
-//       {!isOwnerPath && <Navber />}
-
-//       {/* Optional: Hotel registration */}
-//       {/* <HotelReg /> */}
-
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/rooms" element={<AllRooms />} />
-//         <Route path="/rooms/:id" element={<RoomDetails />} />
-//         <Route path="/my-bookings" element={<MyBookings />} />
-
-//         {/* Owner layout */}
-//         <Route path="/owner/*" element={<Layout />} />
-//         <Route index element={<Dashboard/>} />
-//         <Route path="add-room" element={<AddRoom/>} />
-//         <Route path="list-room" element={<ListRoom/>} />
-//       </Routes>
-
-//       {/* Footer only for user side */}
-//       {!isOwnerPath && <Footer />}
-//     </>
-//   );
-// };
-
-// export default App;
 
 import { Routes, Route, useLocation } from "react-router-dom";
 import Navber from "./components/Navber";
@@ -56,32 +11,34 @@ import Layout from "./components/pages/hotelOwner/Layout";
 import Dashboard from "./components/pages/hotelOwner/Dashboard";
 import AddRoom from "./components/pages/hotelOwner/AddRoom";
 import ListRoom from "./components/pages/hotelOwner/ListRoom";
+import HotelReg from "./components/HotelReg";
+import { Toaster } from "react-hot-toast";
+import { useAppContext } from "./context/AppContext";
 
 const App = () => {
   const location = useLocation();
   const isOwnerPath = location.pathname.startsWith("/owner");
+  const { showHotelReg } = useAppContext();
 
   return (
     <>
-      {/* Navbar only for user side */}
+      <Toaster />
       {!isOwnerPath && <Navber />}
-
+      {showHotelReg && <HotelReg />}
       <Routes>
-        {/* User routes */}
+        {/* User Routes */}
         <Route path="/" element={<Home />} />
         <Route path="/rooms" element={<AllRooms />} />
         <Route path="/rooms/:id" element={<RoomDetails />} />
         <Route path="/my-bookings" element={<MyBookings />} />
 
-        {/* Owner routes (NESTED) */}
+        {/* Owner Routes */}
         <Route path="/owner" element={<Layout />}>
           <Route index element={<Dashboard />} />
           <Route path="add-room" element={<AddRoom />} />
           <Route path="list-room" element={<ListRoom />} />
         </Route>
       </Routes>
-
-      {/* Footer only for user side */}
       {!isOwnerPath && <Footer />}
     </>
   );
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,11 @@
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import{BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { ClerkProvider } from '@clerk/clerk-react'
-
+import { AppProvider } from './context/AppContext.jsx'
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -14,9 +15,13 @@ if (!PUBLISHABLE_KEY) {
 }
 
 createRoot(document.getElementById('root')).render(
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  </ClerkProvider>,
+  <StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <BrowserRouter>
+        <AppProvider>
+          <App />
+        </AppProvider>
+      </BrowserRouter>
+    </ClerkProvider>
+  </StrictMode>
 )
